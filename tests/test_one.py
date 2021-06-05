0cbaabda4b76170e3f2ca8a7140db6e8853f3fb6
nyck33 <nobutaka@gatech.edu>
2021-06-05 15:34:33 -0700

    passed first pytest

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,7 +1,11 @@
 from dash.testing.application_runners import import_app
 
 def test_one(dash_duo):
-    app = import_app("tests.test_app")
+    app = import_app("tests.app")
     dash_duo.start_server(app)
-    dash_duo.wait_for_text_to_equal("h2", "Needleman-Wunsch", timeout=4)
+    dash_duo.wait_for_text_to_equal("h2", "Needleman-Wunsch, Smith-Waterman and Entrez", timeout=4)
+    #assert dash_duo.find_element("h3").text == "Objective"
+    assert dash_duo.find_element("h3").text == "PAM BLOSUM + NCBI"
+    assert dash_duo.get_logs() == [], "Browser console should contain no errors"
 
+    return None
